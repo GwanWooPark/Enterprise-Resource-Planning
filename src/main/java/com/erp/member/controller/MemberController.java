979b874422e81eb02958d7978c733337e766952f
GwanWooPark <pgw4712@icloud.com>
2022-03-14 20:31:30 +0900

    feat: 멤버 회원가입 구현

--- src/main/java/com/erp/member/controller/MemberController.java
+++ src/main/java/com/erp/member/controller/MemberController.java
@@ -1,16 +1,23 @@
 package com.erp.member.controller;
 
+import com.erp.member.controller.dto.MemberSingUpDto;
 import com.erp.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
-@RequestMapping("/members")
+@RequestMapping("/member")
 @Controller
 public class MemberController {
 
     private final MemberService memberService;
 
+    @GetMapping("/signUp")
+    public String signUp(@ModelAttribute(name = "member") MemberSingUpDto memberSingUpDto) {
+        return "member/signUp";
+    }
 
 }
